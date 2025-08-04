f978fd64243ef48ec8ac308805382157cc80882e
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-08-04 08:16:51 +0530

    clicking on btn should start recording

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,43 @@
 'use client'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { motion } from 'motion/react'
+import { cn } from '@/lib/utils/cn';
 
 const Home = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const mediaRecorderRef = useRef(null);
+
+  const handleStartRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true,
+      });
+
+      mediaRecorderRef.current = new MediaRecorder(stream);
+      mediaRecorderRef.current.start();
+
+      setIsRecording(true);
+      console.log('Recording started!');
+    } catch (err) {
+      setIsRecording(false);
+      console.error('Error starting recording: ' + err.message);
+    }
+  };
+
   return (
     <div className='h-screen w-full grid place-items-center'>
       <motion.button
-        className='px-5 py-2 bg-orange-600 rounded-lg cursor-pointer'
+        className={cn(
+          'px-5 py-2  rounded-lg cursor-pointer',
+          isRecording ? 'bg-orange-600' : 'bg-green-600'
+        )}
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 1 }}
-      >Start Recording</motion.button>
+        onClick={handleStartRecording}
+      >
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </motion.button>
     </div>
   )
 }
