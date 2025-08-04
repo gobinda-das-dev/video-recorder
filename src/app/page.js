54e7c80b093fcf43852844f7d5664958406441e7
Gobinda Das <144037976+gobinda-das-dev@users.noreply.github.com>
2025-08-04 08:06:38 +0530

    add start recording btn

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
+'use client'
 import React from 'react'
+import { motion } from 'motion/react'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='h-screen w-full grid place-items-center'>
+      <motion.button
+        className='px-5 py-2 bg-orange-600 rounded-lg cursor-pointer'
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 1 }}
+      >Start Recording</motion.button>
     </div>
   )
 }
